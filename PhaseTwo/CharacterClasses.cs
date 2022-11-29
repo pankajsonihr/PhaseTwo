6e235e5aa6e41b4a8a6899d765ca73e4e9847d1f
Nicholas St-Jacques <nick_stj@live.ca>
2022-11-28 21:03:50 -0500

    New Character Classes

--- PhaseTwo/CharacterClasses.cs
+++ PhaseTwo/CharacterClasses.cs
@@ -1,26 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PhaseTwo
 {
-    abstract class Hero : Player
+    //abstract class Hero : Player
+    //{
+    //    protected EquipedItems _equipedItems;
+    //    protected Hero() : base(30, 12, 9, 16, 8, 25, 9) { _name = "Gimli Lord of the Glittering Caves"; _class = HeroClasses.ValKery; _equipedItems = new EquipedItems(2, 14f, 10f); }
+    //    protected Hero(int strength, int defence, int intelligence, int vitality, int luck, int weaponUse, int dodge) : base(strength, defence, intelligence, vitality, luck, weaponUse, dodge) { _name = "Hero"; _class = HeroClasses.ValKery; _equipedItems = new EquipedItems(1, 8f, 6f); }
+    //}
+
+    class Knight : Player
     {
         protected EquipedItems _equipedItems;
-        public Hero() : base(10,10,10,10,10,10,10) { _name = "Generic Hero 2,147,483,647"; _class = HeroClasses.Hero; _equipedItems = new EquipedItems(1, 6f, 6f); }
-        public Hero(int strength, int defence, int intelligence, int vitality, int luck, int weaponUse, int dodge) : base(strength, defence, intelligence, vitality, luck, weaponUse, dodge) { _name = "Hero"; _class = HeroClasses.Hero; _equipedItems = new EquipedItems(1, 6f, 6f); }
-
-        public abstract bool EquipWeapon(int itemIndex);
-
-        public string GetEquippedWeapons()
-        {
-            return _equipedItems.ToString();
-        }
-    }
-    class Knight : Hero
-    {
         public Knight() : base(25, 18, 7, 13, 5, 25, 7) { _name = "Théoden Lord of the Mark"; _class = HeroClasses.Knight; _equipedItems = new EquipedItems(1, 8f, 6f); }
         public Knight(int strength, int defence, int intelligence, int vitality, int luck, int weaponUse, int dodge) : base(strength, defence, intelligence, vitality, luck, weaponUse, dodge) { _name = "Knight"; _class = HeroClasses.Knight; _equipedItems = new EquipedItems(1, 8f, 6f); }
         public override bool EquipWeapon(int itemIndex)
@@ -45,9 +41,14 @@ namespace PhaseTwo
             }
             return false;
         }
+        public override string GetEquippedWeapons()
+        {
+            return _equipedItems.ToString();
+        }
     }
-    class Wizard : Hero
+    class Wizard : Player
     {
+        protected EquipedItems _equipedItems;
         public Wizard() : base(15, 13, 25, 11, 9, 14, 13) { _name = "Saruman Lord of Isengard"; _class = HeroClasses.Wizard; _equipedItems = new EquipedItems(1, 6f, 8f); }
         public Wizard(int strength, int defence, int intelligence, int vitality, int luck, int weaponUse, int dodge) : base(strength, defence, intelligence, vitality, luck, weaponUse, dodge) { _name = "Wizard"; _class = HeroClasses.Wizard; _equipedItems = new EquipedItems(1, 6f, 8f); }
 
@@ -73,9 +74,15 @@ namespace PhaseTwo
             }
             return false;
         }
+
+        public override string GetEquippedWeapons()
+        {
+            return _equipedItems.ToString();
+        }
     }
-    class ValKery : Hero
+    class ValKery : Player
     {
+        protected EquipedItems _equipedItems;
         public ValKery() : base(30, 12, 9, 16, 8, 25, 9) { _name = "Gimli Lord of the Glittering Caves"; _class = HeroClasses.ValKery; _equipedItems = new EquipedItems(2, 14f, 10f); }
         public ValKery(int strength, int defence, int intelligence, int vitality, int luck, int weaponUse, int dodge) : base(strength, defence, intelligence, vitality, luck, weaponUse, dodge) { _name = "ValKery"; _class = HeroClasses.ValKery; }
 
@@ -101,16 +108,24 @@ namespace PhaseTwo
             }
             return false;
         }
+
+        public override string GetEquippedWeapons()
+        {
+            return _equipedItems.ToString();
+        }
     }
 
     class EquipedItems : Inventory
     {
         public EquipedItems(int maxCount, float maxWeight, float maxVolume) : base(maxCount,maxWeight,maxVolume) { }
+
+        public EquipedWeapon();
+
     }
 
     static class EquipementTester
     {
-        public static void AddEquipment(Hero player)
+        public static void AddEquipment(Player player)
         {
             bool addmore = true;
             do
@@ -152,5 +167,15 @@ namespace PhaseTwo
         }
     }
 
-    enum HeroClasses { Hero, Knight, Wizard, ValKery };
+    public struct AllowedWeapons
+    {
+        public static readonly Dictionary<HeroClasses, List<InventoryItem>> AllowedWeaponsDict = new Dictionary<HeroClasses, List<InventoryItem>>
+        {
+            { HeroClasses.Knight, new List<InventoryItem>(){ new Sword() } },
+            { HeroClasses.Wizard, new List<InventoryItem>(){ new Staff() } },
+            { HeroClasses.ValKery, new List<InventoryItem>(){ new Axe(), new Shield() } }
+        };
+    }
+
+    enum HeroClasses { Knight, Wizard, ValKery };
 }
